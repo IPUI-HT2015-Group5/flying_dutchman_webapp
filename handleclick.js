cf01e1de156362ad4a12a9365f8cc8a96ae8f664
Giovanni Santini <giovannisantini93@yahoo.it>
2015-11-25 16:13:19 +0100

    More stupid work, yuppie :D

---
+++ handleclick.js
@@ -0,0 +1,13 @@
+/**
+ * Created by itachisan on 25/11/15.
+ */
+function handleIt(user, pass) {
+    alert("hello");
+    //JSON http://pub.jamaica-inn.net/fpdb/api.php?username=aamsta&password=aamsta&action=iou_get_all
+}
+
+function checkUsers() {
+    var users = "blablabla";
+    if (users.has(key))
+        alert("hello")
+}
