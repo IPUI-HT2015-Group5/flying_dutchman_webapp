7d234aac77fc5c6154ff8ac8a9e1186d9a4b4c27
Magto <martin@magto.se>
2015-12-07 10:07:46 +0100

    Added the translator

--- Logon.js
+++ Logon.js
@@ -2,34 +2,5 @@
 
 $("form").submit(function(event){
     event.preventDefault();
-        alert("test")
-        var action = iou_get;
-        var admins = ["jorass", "ervtod", "hirchr", "saskru", "svetor"];
-        var PubAPI = "http://pub.jamaica-inn.net/fpdb/api.php?username=" + username + "&password=" + password + "&action=" + action;
-
-        localStorage.setItem("user", username);
-
-        $.getJSON(PubAPI)
-            .done(function (data) {
-                $.each(data.payload, function (i, value) {
-
-                    localStorage.setItem("assets", value.assets);
-                    localStorage.setItem("firstName", value.first_name);
-                    localStorage.setItem("lastName", value.last_name);
-
-                    switch (value.type) {
-                        case error:
-                            alert(value.msg);
-
-                        default:
-                            if ($.inArray(username, admins) > -1){
-                                window.location.href = 'admins.html';
-                                return false;
-                            } else {
-                                window.location.href = 'customer.html';
-                                return false;
-                            }
-                    }
-                });
-            });
+    logon(username, password);
 });
