cefd809102c6b8a3d78efe41cd6e8e2abd6e4f30
soma6970 <soma6970@student.uu.se>
2015-12-08 16:32:51 +0100

    More-working undo. Can undo - adding but not removing. A redo that in theory works but needs a functioning undo :)

--- scripts/shopping.js
+++ scripts/shopping.js
@@ -52,7 +52,7 @@ function addBeer(ev,dropElementID) {
         );
     }
 
-    countclick(ev, number,dropElementID);
+    countclick("addBeer",dropElementID, movedID);
 }
 
 /**
@@ -62,6 +62,7 @@ function addBeer(ev,dropElementID) {
  * Used to not delete wrong stuff.
  */
 function removeBeer(ev, parentID) {
+
     // Prevent default behaviour
     ev.preventDefault();
     // Get the button data and its specific number
@@ -79,38 +80,147 @@ function removeBeer(ev, parentID) {
         $("#" + movedID).remove();
         $("#" + oldDivID).remove();
     }
+    //alert(ev + "removeBeer"+number+ parentID + movedID)
+    countclick("removeBeer", parentID, movedID);
 }
 
-function iObject() {
-    this.i;
-    return this;
-}
 
-var myObject=new iObject();
-myObject.i=0;
-var myObject2=new iObject();
-myObject2.i=0;
-array=new Array();
+store=[]
+store_undo=[]
 
 
-function countclick(tag, ID, parentID) {
-    myObject.i++;
-    var y=myObject.i;
-    var x=ID;
-    array[y]=x;
-    parentID=array;
-    alert("Countclick" + parentID);
-    return parentID;
+function countclick(funcName, parentID, movedID) {
+    store.unshift([funcName,parentID, movedID]);
+    //alert (store);
+    alert("Store: "+ store + "  Inviterad store: " + store_undo);
+
+    return ;
 
 }
 
-function undo(tag,parentID) {
+function undo(ev) {
+    alert("Store: "+ store + "  Inviterad store: " + store_undo);
+    if (store[0][0]=="addBeer"){
+        var parentID=store[0][1];
+        var movedID=store[0][2];
+        var movedID=movedID.concat("_0");
+        var movedID=movedID.replace("beerButton","beerToBuyButton");
+        var number = movedID.replace("beerToBuyButton", "");
+        var oldDivID = "beerToBuyButtonContainer" + number;
+        //alert([number, parentID, movedID])
+        /* Delete the elements, just if the correct parent is the one we look for
+         Actually, the structure is like:
+         list -> container div -> button
+         So we should check for the "grandparent" (parent's parent).
+         */
+        if ($("#" + movedID).parent().parent().prop("id") == parentID) {
+            $("#" + movedID).remove();
+            $("#" + oldDivID).remove();
+        }
 
-    var z=array.length;
-    array.pop();
-alert ("undo" + array);
+    }
+    else if (store[0][0]=="removeBeer") {
+        var dropElementID=store[0][1];
+        var movedID=store[0][2];
+
+        var movedID=movedID.replace("_0","");
+        var movedID=movedID.replace("beerToBuyButton","beerButton");
+        var number = movedID.replace("beerButton", "");
+
+        // Create the new IDs
+        var divID = "beerButtonContainer" + number;
+
+        // Allow multiple selection of the same beer
+        var newDivID = "beerToBuyButtonContainer" + number + "_" +
+            findNextNumber("beerToBuyButtonContainer" + number + "_", 0);
+        var newButtonID = "beerToBuyButton" + number + "_" +
+            findNextNumber("beerToBuyButton" + number + "_", 0);
+
+        // Drop just in the correct element (check done with the ID)
+        var tempDivID = "beers-to-buy"+number;
+        $(tempDivID).append(
+            $("#" + divID).clone().empty().prop("id", newDivID)
+        );
+        $("#" + newDivID).append(
+            $("#" + movedID).clone().prop("id", newButtonID)
+        );
+
+
+        /*
+        if(event.target.id == dropElementID) {
+            // Create a new container div...
+            $(ev.target).append(
+                $("#" + divID).clone().empty().prop("id", newDivID)
+            );
+            // And add the new one!
+            $("#" + newDivID).append(
+                $("#" + movedID).clone().prop("id", newButtonID)
+            );
+        }
+        */
+    }
+  //  alert(store)
+    store_undo.unshift(store[0]);
+    store.shift();
+  // alert(store)
 }
 
-function redo(tag) {
-alert("redo")
+function redo(ev) {
+    alert("Store: "+ store + "  Inviterad store: " + store_undo);
+    if (store_undo[0][0]=="addBeer"){
+        var parentID=store_undo[0][1];
+        var movedID=store_undo[0][2];
+        var number = movedID.replace("beerButton", "");
+        // Create the new IDs
+        var divID = "beerButtonContainer" + number;
+
+        // Allow multiple selection of the same beer
+        var newDivID = "beerToBuyButtonContainer" + number + "_" +
+            findNextNumber("beerToBuyButtonContainer" + number + "_", 0);
+        var newButtonID = "beerToBuyButton" + number + "_" +
+            findNextNumber("beerToBuyButton" + number + "_", 0);
+
+        // Drop just in the correct element (check done with the ID)
+        if(event.target.id == dropElementID) {
+            // Create a new container div...
+            $(ev.target).append(
+                $("#" + divID).clone().empty().prop("id", newDivID)
+            );
+            // And add the new one!
+            $("#" + newDivID).append(
+                $("#" + movedID).clone().prop("id", newButtonID)
+            );
+        }
+
+    }
+    else if (store_undo[0][0]=="removeBeer") {
+        var dropElementID=store_undo[0][1];
+        var movedID=store_undo[0][2];
+
+        var number = movedID.replace("beerButton", "");
+        // Create the new IDs
+        var divID = "beerButtonContainer" + number;
+
+        // Allow multiple selection of the same beer
+        var newDivID = "beerToBuyButtonContainer" + number + "_" +
+            findNextNumber("beerToBuyButtonContainer" + number + "_", 0);
+        var newButtonID = "beerToBuyButton" + number + "_" +
+            findNextNumber("beerToBuyButton" + number + "_", 0);
+
+        // Drop just in the correct element (check done with the ID)
+        if(event.target.id == dropElementID) {
+            // Create a new container div...
+            $(ev.target).append(
+                $("#" + divID).clone().empty().prop("id", newDivID)
+            );
+            // And add the new one!
+            $("#" + newDivID).append(
+                $("#" + movedID).clone().prop("id", newButtonID)
+            );
+        }
+    }
+    alert(store);
+    alert(store_undo);
+    store.unshift(store_undo[0]);
+    store_undo.shift();
 }
